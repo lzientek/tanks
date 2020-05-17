b152f4149ec869f4abc32887bf51632d53a1ad7a
lucas zientek <152565@supinfo.com>
2020-05-17 19:52:57 +0200

    working turret

--- src/containers/Tank.ts
+++ src/containers/Tank.ts
@@ -24,30 +24,33 @@ export class Tank extends Phaser.GameObjects.Container {
         this.body.setCollideWorldBounds(true);
     }
 
-    tankMoves(cursors): void {
+    tankMoves(cursors: Phaser.Types.Input.Keyboard.CursorKeys): void {
+        this.bodyMoves(cursors);
+        this.turretMoves();
+    }
+
+    private bodyMoves(cursors: Phaser.Types.Input.Keyboard.CursorKeys): void {
         this.body.setAngularVelocity(0);
         this.body.setVelocity(0);
-
         if (cursors.left.isDown) {
             this.body.setAngularVelocity(-200);
         } else if (cursors.right.isDown) {
             this.body.setAngularVelocity(200);
         }
-
         if (cursors.up.isDown) {
             this.body.velocity.copy(this.scene.physics.velocityFromAngle(this.angle - 90, 100)); //+90 to start at the front of the sprite
         } else if (cursors.down.isDown) {
-            this.body.velocity.copy(this.scene.physics.velocityFromAngle(this.angle + 90, 100)); //-90 to go backwards
+            this.body.velocity.copy(this.scene.physics.velocityFromAngle(this.angle + 90, 100));
         }
-
-        this.turretMoves();
     }
 
-    turretMoves(): void {
+    private turretMoves(): void {
         const mousePosition = this.scene.input.mousePointer.position;
-
+        console.log(this.angle, Phaser.Math.DegToRad(this.angle));
         this.turret.setRotation(
-            Phaser.Math.Angle.Between(mousePosition.x, mousePosition.y, this.x, this.y) - Math.PI / 2,
+            Phaser.Math.Angle.Between(mousePosition.x, mousePosition.y, this.x, this.y) -
+                Phaser.Math.DegToRad(this.angle) -
+                Math.PI / 2,
         );
     }
 }
