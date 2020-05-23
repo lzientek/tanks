8d289e69bf1da39233d9200559fe0039c001304e
lucas zientek <152565@supinfo.com>
2020-05-23 22:55:26 +0200

    improve network solicitation from socket

--- front/src/socket/Client.ts
+++ front/src/socket/Client.ts
@@ -2,12 +2,18 @@ import io from 'socket.io-client';
 
 export default class Client {
     socket: SocketIOClient.Socket;
+
     constructor() {
         this.socket = io.connect('http://localhost:3001');
     }
 
+    previousX: number;
+    previousY: number;
     move(x: number, y: number): void {
-        console.log(this);
-        this.socket.emit('move', { x, y });
+        if (x !== this.previousX || y !== this.previousY) {
+            this.socket.emit('move', { x, y });
+            this.previousY = y;
+            this.previousX = x;
+        }
     }
 }
